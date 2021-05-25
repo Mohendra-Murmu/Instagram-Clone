9b59d0ab3221fd95c17b2ee308b3a4cebf8c1f39
Mr. Nobody <mahendradipu@gmail.com>
2021-05-25 11:06:47 +0530

    2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 
 
 const config = {
-  apiKey: "AIzaSyAtw-PXhJQmGYix-Md8oChT8p-KFPXVWrM",
-  authDomain: "instagram-clone016.firebaseapp.com",
-  databaseURL: "https://instagram-clone016-default-rtdb.firebaseio.com",
-  projectId: "instagram-clone016",
-  storageBucket: "instagram-clone016.appspot.com",
-  messagingSenderId: "403015998484",
-  appId: "1:403015998484:web:1f9a94ad72dc3a4b875b01",
-  measurementId: "G-BC6QD9J6B3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 };
 
